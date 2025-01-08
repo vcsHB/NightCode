7a72f2043f457afb80dde94c45e94aaa5f383d23
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-01-08 15:24:13 +0900

    feat: Set Player In Scene

---
+++ Assets/01.Scripts/Agent/Player/EventController.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Agents.Players
+{
+
+    public class FeedbackEventController : MonoBehaviour
+    {
+        //private Dictionary<string, 
+
+    }
+
+}
