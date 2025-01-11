e92dddf1890fe9f12a5446b26c43b02a855c6f4b
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-01-11 23:01:23 +0900

    feat: CameraControllers

---
+++ Assets/01.Scripts/CameraController/CameraManager.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace CameraControllers
+{
+
+    public class CameraManager : MonoSingleton<CameraManager>
+    {
+        [SerializeField] private CinemachineCamera _camera;
+        private Dictionary<Type, ICameraControlable> _controllers = new Dictionary<Type, ICameraControlable>();
+
+
+        private void Start()
+        {
+            GetComponentsInChildren<ICameraControlable>(true)
+               .ToList().ForEach(controller => _controllers.Add(controller.GetType(), controller));
+        }
+
+
+        public T GetCompo<T>(bool isDerived = false) where T : class
+        {
+            if (_controllers.TryGetValue(typeof(T), out ICameraControlable compo))
+            {
+                return compo as T;
+            }
+
+            if (!isDerived) return default;
+
+            Type findType = _controllers.Keys.FirstOrDefault(x => x.IsSubclassOf(typeof(T)));
+            if (findType != null)
+                return _controllers[findType] as T;
+
+            return default(T);
+        }
+
+
+
+
+    }
+}
