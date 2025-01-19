f805632ddf489edf03c9e7ba1b472deb91e1b4f8
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-01-19 22:28:36 +0900

    feat: Pause Icon Shader

---
+++ Assets/01.Scripts/UI/InGame/PausePanel.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace UI.InGame.SystemUI
+{
+
+    public class PausePanel : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
