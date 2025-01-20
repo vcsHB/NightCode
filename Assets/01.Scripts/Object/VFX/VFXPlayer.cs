622058f95d6d3198f2b9d4e36c75ff61c7cf6bf3
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-01-20 19:09:14 +0900

    feat: Quest System

---
+++ Assets/01.Scripts/Object/VFX/VFXPlayer.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace ObjectManage
+{
+    public class VFXPlayer : MonoBehaviour
+    {
+        
+    }
+
+}
---
+++ Assets/01.Scripts/QuestSystem/QuestManager.cs
@@ -0,0 +1,71 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+namespace QuestSystem
+{
+    [System.Serializable]
+    public struct QuestData
+    {
+        public float goalProgress;
+        public float currentProgress;
+    }
+
+    public class QuestManager : MonoBehaviour
+    {
+        [Header("Events Setting")]
+        public UnityEvent OnQuestCompleteEvent;
+        public event Action<float, float> OnQuestProgressChangedEvent; // <Progress, GoalProgress>
+
+        [Header("Essential Setting")]
+        [SerializeField] private QuestListSO _listSO;
+        [Header("Current Status")]
+        [field: SerializeField] public QuestSO CurrentQuest { get; private set; }
+        [SerializeField] private QuestData _currentQuestData;
+        public QuestData CurrentQuestData => _currentQuestData;
+        public bool IsComplete => _currentQuestData.currentProgress >= _currentQuestData.goalProgress;
+
+
+
+        public void Initialize()
+        {
+
+        }
+
+
+        public void ChangeQuest(QuestSO questSO)
+        {
+            CurrentQuest = questSO;
+            _currentQuestData.currentProgress = 0f;
+            _currentQuestData.goalProgress = CurrentQuest.goalProgress;
+            InvokeQuestProgressChanged();
+
+            // 퀘스트 타입에 따른 추가적인 수행처리
+            switch (questSO.questType)
+            {
+                case QuestType.KillSingleTarget:
+                    break;
+                case QuestType.KillMultiTarget:
+                    break;
+                case QuestType.KillAll:
+                    break;
+                case QuestType.Rescue:
+                    break;
+            }
+        }
+
+        public void IncreaseProgress(float amount)
+        {
+            _currentQuestData.currentProgress += amount;
+            InvokeQuestProgressChanged();
+            if (IsComplete)
+            {
+                OnQuestCompleteEvent?.Invoke();
+            }
+        }
+
+        private void InvokeQuestProgressChanged()
+        {
+            OnQuestProgressChangedEvent?.Invoke(_currentQuestData.currentProgress, _currentQuestData.goalProgress);
+        }
+    }
+}
