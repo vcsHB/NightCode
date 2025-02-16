5c739079095027c9089653a5cb65c82ce2cbb984
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-02-17 06:32:38 +0900

    feat: Air Attack Animation, Attack Type

--- Assets/01.Scripts/Agent/Katana/KatanaPlayerAnimationTrigger.cs
+++ Assets/01.Scripts/Agent/Katana/KatanaPlayerAnimationTrigger.cs
@@ -1,10 +1,22 @@
 using Agents.Players;
-using UnityEngine;
+using UnityEngine.Events;
 
 namespace Agents.Animate
 {
     public class KatanaPlayerAnimationTrigger : PlayerAnimationTrigger
     {
+        public UnityEvent OnAirAttack1Event;
+        public UnityEvent OnAirAttack2Event;
+
+        public override void HandleAirAttack1()
+        {
+            OnAirAttack1Event?.Invoke();
+        }
+
+        public override void HandleAirAttack2()
+        {
+            OnAirAttack2Event?.Invoke();
+        }
         
     }
 
--- Assets/01.Scripts/Agent/Player/PlayerAnimationTrigger.cs
+++ Assets/01.Scripts/Agent/Player/PlayerAnimationTrigger.cs
@@ -4,7 +4,7 @@ using UnityEngine.Events;
 namespace Agents.Players
 {
 
-    public class PlayerAnimationTrigger : MonoBehaviour, IAgentComponent
+    public abstract class PlayerAnimationTrigger : MonoBehaviour, IAgentComponent
     {
         public UnityEvent OnFirstAttackEvent;
         public UnityEvent OnSecondAttackEvent;
@@ -37,6 +37,9 @@ namespace Agents.Players
         {
             OnSecondAttackEvent?.Invoke();
         }
+
+        public abstract void HandleAirAttack1();
+        public abstract void HandleAirAttack2();
 
 
     }
