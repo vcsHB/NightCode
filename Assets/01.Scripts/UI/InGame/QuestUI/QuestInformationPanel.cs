e1e60a7ecf1373425c6f8950829835d208450940
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-02-27 22:04:22 +0900

    feat: Quest Panel

---
+++ Assets/01.Scripts/UI/InGame/QuestUI/QuestInformationPanel.cs
@@ -0,0 +1,14 @@
+using System.IO;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+namespace UI.InGame.GameUI
+{
+    public class QuestInformationPanel : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _questNameText;
+        [SerializeField] private Slider _progressGauge;
+
+        
+    }
+}
---
+++ Assets/01.Scripts/UI/InGame/QuestUI/QuestPanel.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+
+namespace UI.InGame.GameUI.QuestSyetem
+{
+    public class QuestPanel : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _titleText;
+
+    }
+}
