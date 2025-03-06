cb9c1fcc1dea1fe25866841c762a803233d8f96b
Leooo <123617759+Leooo918@users.noreply.github.com>
2025-03-06 17:18:14 +0900

    feat: b

--- Assets/01.Scripts/Basement/Room/Office.cs
+++ Assets/01.Scripts/Basement/Room/Office.cs
@@ -6,18 +6,25 @@ namespace Basement
     public class Office : BasementRoom
     {
         public MissionSelectPanel missionPanel;
+        public CharacterSelectPanel selectaPanel;
         public Furniture table;
 
         protected override void Awake()
         {
             base.Awake();
             table.Init(this);
-            table.InteractAction += missionPanel.Open;
+            table.InteractAction += InteractTable;
         }
 
         private void OnDisable()
         {
-            table.InteractAction -= missionPanel.Open;
+            table.InteractAction -= InteractTable;
+        }
+
+        private void InteractTable()
+        {
+            missionPanel.Open();
+            selectaPanel.Open(Vector2.zero);
         }
 
         public override void FocusRoom()
