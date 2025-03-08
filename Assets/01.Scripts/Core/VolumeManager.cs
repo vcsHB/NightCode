9f6aaaf74facd53f95d6f5b1cc4920e0b1258ac8
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-03-08 21:48:30 +0900

    feat: Lens Distortion Effect, Feedback

--- Assets/01.Scripts/Core/VolumeManager.cs
+++ Assets/01.Scripts/Core/VolumeManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
@@ -12,12 +14,16 @@ namespace Core
     {
         [SerializeField] private Volume _globalVolume;
         private ChromaticAberration _chromaticEffect;
+        private LensDistortion _lensDistortion;
+        private bool _isLensEffectTweening;
+        private readonly float _defaultLensDistortionLevel;
 
 
         protected override void Awake()
         {
             base.Awake();
             _globalVolume.profile.TryGet(out _chromaticEffect);
+            _globalVolume.profile.TryGet(out _lensDistortion);
         }
 
         public void HandleEnableChromatic()
@@ -35,6 +41,45 @@ namespace Core
         {
             _chromaticEffect.intensity.value = value;
 
+        }
+
+        public void SetDefaultLensDistortion()
+        {
+            SetLensDistortion(_defaultLensDistortionLevel);
+        }
+        public void SetLensDistortion(float intensity)
+        {
+            _lensDistortion.intensity.value = intensity;
+        }
+        public void SetLensDistortion(float intensity, float tweenDuration, float term)
+        {
+            if (_isLensEffectTweening) return;
+            StartCoroutine(LensDistortionCoroutine(intensity, tweenDuration, term));
+        }
+        private IEnumerator LensDistortionCoroutine(float intensity, float tweenDuration, float term)
+        {
+            _isLensEffectTweening = true;
+            float currentTime = 0f;
+            float defaultValue = _lensDistortion.intensity.value;
+            while (currentTime < tweenDuration)
+            {
+                currentTime += Time.deltaTime;
+                float ratio = currentTime / tweenDuration;
+                SetLensDistortion(Mathf.Lerp(defaultValue, intensity, ratio));
+                yield return null;
+            }
+            SetLensDistortion(intensity);
+            yield return new WaitForSeconds(term);
+            currentTime = 0f;
+            while (currentTime < tweenDuration)
+            {
+                currentTime += Time.deltaTime;
+                float ratio = currentTime / tweenDuration;
+                SetLensDistortion(Mathf.Lerp(defaultValue, intensity, 1f - ratio));
+                yield return null;
+            }
+            SetLensDistortion(defaultValue);
+            _isLensEffectTweening = false;
         }
 
 
