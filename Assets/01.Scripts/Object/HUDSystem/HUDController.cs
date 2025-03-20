8a6907f41349202521571b62bedf21d4e5941d95
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-03-20 19:35:08 +0900

    feat: HUD System

---
+++ Assets/01.Scripts/Object/HUDSystem/HUDController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+namespace HUDSystem
+{
+
+    public class HUDController : MonoBehaviour
+    {
+        [SerializeField] private Transform _followTarget;
+        private HUDObject[] _hudList;
+
+        private bool _isFollow;
+
+        private void Awake()
+        {
+            _hudList = GetComponentsInChildren<HUDObject>();
+        }
+
+        private void Update()
+        {
+
+            Follow();
+        }
+
+        public void SetFollowTarget(Transform newTarget)
+        {
+            _followTarget = newTarget;
+        }
+        private void Follow()
+        {
+            if (!_isFollow) return;
+
+            transform.position = _followTarget.position;
+
+        }
+
+        public T GetHUD<T>() where T : class
+        {
+            foreach (var hud in _hudList)
+            {
+                if (hud is T)
+                {
+                    return hud as T;
+                }
+            }
+            return default(T);
+        }
+    }
+
+}
