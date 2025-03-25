39675d169f10d6a8d97c62de89f692ac2e6bede1
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-03-25 12:03:53 +0900

    feat: Description Panel

--- Assets/01.Scripts/Tutorial/UI/DescriptionPanel.cs
+++ Assets/01.Scripts/Tutorial/UI/DescriptionPanel.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,7 +11,31 @@ namespace UI.Tutorial
         [SerializeField] private Image _topLineImage;
         [SerializeField] private Image _bottomeLineImage;
         private int _lineUnscaledTimeHash = Shader.PropertyToID("_CurrentUnscaledTime");
+        [SerializeField] private Image _waitGauge;
+        [SerializeField] private Button _closeButton;
+        [SerializeField] private float _closeWaitDuration = 3f;
 
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _closeButton.onClick.AddListener(Close);
+
+        }
+        public override void Open()
+        {
+            base.Open();
+            _closeButton.interactable = false;
+            _waitGauge.fillAmount = 0f;
+            _waitGauge.DOFillAmount(1f, _closeWaitDuration).SetUpdate(true).OnComplete(() => _closeButton.interactable = true);
+            Time.timeScale = 0f;
+        }
+
+        public override void Close()
+        {
+            base.Close();
+            Time.timeScale = 1f;
+        }
         private void Update()
         {
 
@@ -19,6 +45,8 @@ namespace UI.Tutorial
                 _bottomeLineImage.material.SetFloat(_lineUnscaledTimeHash, Time.unscaledTime);
             }
         }
+
+
     }
 
 }
