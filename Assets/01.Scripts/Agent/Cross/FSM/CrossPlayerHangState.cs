f72cca1d53c9108649fd4915d31a4e22e5c4ae8d
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-04-03 08:54:16 +0900

    fix: swap Cresent Cross Attack

--- Assets/01.Scripts/Agent/Cross/FSM/CrossPlayerHangState.cs
+++ Assets/01.Scripts/Agent/Cross/FSM/CrossPlayerHangState.cs
@@ -5,13 +5,25 @@ namespace Agents.Players.FSM
 
     public class CrossPlayerHangState : PlayerHangState
     {
+        private CrossPlayerAnimationTrigger _crossAnimTrigger;
         public CrossPlayerHangState(Player player, PlayerStateMachine stateMachine, AnimParamSO animParam) : base(player, stateMachine, animParam)
         {
+            _crossAnimTrigger = _player.GetCompo<CrossPlayerAnimationTrigger>();
+            
         }
         public override void UpdateState()
         {
             base.UpdateState();
 
+        }
+
+        protected override void HandleUseTurbo()
+        {
+            if (!_player.IsActive) return;
+            if (!_canUseTurbo) return;
+
+            ForceUseTurbo();
+            _crossAnimTrigger.SetReload(true);
         }
     }
 }
