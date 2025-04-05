e681cb3924066f8bd938990d33cdad7ef1425cc1
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-04-05 23:54:52 +0900

    feat: new Office Scene, Interact Controller

---
+++ Assets/01.Scripts/Office/OfficeInteractSystem/InteractController.cs
@@ -0,0 +1,62 @@
+using InputManage;
+using UnityEngine;
+
+namespace Office.InteractSystem
+{
+    public class InteractController : MonoBehaviour
+    {
+        [SerializeField] private UIInputReader _uiInputReader;
+        [SerializeField] private LayerMask _targetLayer;
+        [SerializeField] private float _detectRadius = 1f;
+        private bool _canInteract = true;
+        private IInteractable _interactTarget;
+
+        private void Awake()
+        {
+            _uiInputReader.OnLeftClickEvent += Interact;
+        }
+
+        private void OnDestroy()
+        {
+            _uiInputReader.OnLeftClickEvent -= Interact;
+
+        }
+        public void Interact()
+        {
+            if (!_canInteract) return;
+            if (_interactTarget == null) return;
+            _interactTarget.Interact();
+        }
+
+
+        private void Update()
+        {
+            CheckInteract(Camera.main.ScreenToWorldPoint(_uiInputReader.MousePositionOnScreen));
+
+        }
+
+
+        private void CheckInteract(Vector2 position)
+        {
+            var collider = Physics2D.OverlapCircle(position, _detectRadius, _targetLayer);
+
+            IInteractable newTarget = null;
+            bool hasTarget = collider != null && collider.transform.TryGetComponent(out newTarget);
+
+            if (_interactTarget != null && (!hasTarget || _interactTarget != newTarget))
+            {
+                _interactTarget.HoverExit();
+                _interactTarget = null;
+            }
+
+            if (hasTarget && _interactTarget != newTarget)
+            {
+                _interactTarget = newTarget;
+                _interactTarget.HoverEnter();
+            }
+        }
+
+
+    }
+
+}
