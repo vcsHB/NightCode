e626133d0b5ea8768199a26ee2dd8dffd50df08e
Leooo <123617759+Leooo918@users.noreply.github.com>
2025-04-14 19:56:39 +0900

    springGimmick

--- Assets/01.Scripts/MapGimmick/SpringGimmick.cs
+++ Assets/01.Scripts/MapGimmick/SpringGimmick.cs
@@ -9,18 +9,22 @@ namespace ObjectManage.GimmickObjects.Logics
 
         [SerializeField] private SpriteRenderer _pivot;
         [SerializeField] private Transform _edge;
+        [SerializeField] private Transform _warning;
         [SerializeField] private SpringGimmickBottom _bottom;
 
         [Space]
         [SerializeField] private float _maxDistance;
+        [SerializeField] private float _warningDelay;
         [SerializeField] private float _downDelay;
         [SerializeField] private float _downDuration;
         [SerializeField] private float _upDelay;
         [SerializeField] private float _upDuration;
 
+
         private Sequence _seq;
         private float _prevDown;
         private bool _isDown = false;
+        private bool _isWarninig = false;
 
         private void OnEnable()
         {
@@ -34,10 +38,25 @@ namespace ObjectManage.GimmickObjects.Logics
                 _isDown = true;
                 Down();
             }
+
+            if (!_isWarninig && _prevDown + _warningDelay < Time.time)
+            {
+                _isWarninig = true;
+                Warning();
+            }
+        }
+
+        private void Warning()
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, _maxDistance, _whatIsGround);
+            float distance = hit.distance;
+
+            _warning.localScale = new Vector3(1, distance, 1);
         }
 
         private void Down()
         {
+            _warning.localScale = new Vector3(1, 0, 1);
             RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, _maxDistance, _whatIsGround);
             float distance = hit.distance;
             _bottom.SetIsDown(true);
