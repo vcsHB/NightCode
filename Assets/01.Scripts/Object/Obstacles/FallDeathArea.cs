62737b06a2888a8e42622da696f8ca54bae42e77
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-04-21 17:46:06 +0900

    feat: Fall Area Damage

--- Assets/01.Scripts/Object/Obstacles/FallDeathArea.cs
+++ Assets/01.Scripts/Object/Obstacles/FallDeathArea.cs
@@ -1,3 +1,4 @@
+using Combat;
 using Combat.PlayerTagSystem;
 using UnityEngine;
 using UnityEngine.Events;
@@ -8,16 +9,20 @@ namespace ObjectManage
     public class FallDeathArea : MonoBehaviour
     {
         public UnityEvent OnEnterEvent;
-        [SerializeField] private float _damage;
+        [SerializeField] private CombatData _combatData;
         [SerializeField] private Transform _backPoint;
-
+        private PlayerCombatManager _playerCombatManager;
+        private void Start()
+        {
+            _playerCombatManager = PlayerManager.Instance.GetCompo<PlayerCombatManager>();
+        }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             PlayerManager.Instance.SetCurrentPlayerPosition(_backPoint.position);
 
             OnEnterEvent?.Invoke();
-            
+            _playerCombatManager.ApplyDamagePlayer(_combatData);
         }
 
     }
