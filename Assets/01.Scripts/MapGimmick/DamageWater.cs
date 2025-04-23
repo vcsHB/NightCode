0d04c0ae8d2a1418bd434c2cfda896fa4a18baad
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-04-23 10:47:27 +0900

    feat: Water Filler

--- Assets/01.Scripts/MapGimmick/DamageWater.cs
+++ Assets/01.Scripts/MapGimmick/DamageWater.cs
@@ -5,14 +5,28 @@ namespace Ingame.Gimmick
 {
     public class DamageWater : Caster
     {
+
         [SerializeField] private KnockbackCaster _knockBackCaster;
         [SerializeField] private float _knockbackPower;
+        private SpriteRenderer _visualRenderer;
+        public float CurrentFillLevel => _visualRenderer.size.y;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _visualRenderer = GetComponentInChildren<SpriteRenderer>();
+        }
+
+        public void SetFill(float fillLevel)
+        {
+            _visualRenderer.size = new Vector2(_visualRenderer.size.x, fillLevel);
+        }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
             if (collision.contactCount > 0)
             {
-                Vector2 knockDir = 
+                Vector2 knockDir =
                     (Vector2)collision.collider.transform.position
                     - collision.contacts[0].point;
                 knockDir.x = 0;
