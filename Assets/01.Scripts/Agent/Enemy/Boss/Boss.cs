d51e627d79fb03de92a0fdfb2e0bf446fefee4e9
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-04-25 20:35:23 +0900

    feat: Boss Animation

---
+++ Assets/01.Scripts/Agent/Enemy/Boss/Boss.cs
@@ -0,0 +1,14 @@
+using Combat;
+using Core.EventSystem;
+using Unity.Behavior;
+using UnityEngine;
+
+namespace Agents.Enemies.BossManage
+{
+
+    public class Boss : Enemy
+    {
+       
+    }
+
+}
---
+++ Assets/01.Scripts/Agent/Enemy/Boss/BurnOut/BurnOutBossMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+namespace Agents.Enemies.BossManage
+{
+
+    public class BurnOutBossMovement : MonoBehaviour, IAgentComponent
+    {
+        public void AfterInit()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Initialize(Agent agent)
+        {
+        }
+    }
+}
