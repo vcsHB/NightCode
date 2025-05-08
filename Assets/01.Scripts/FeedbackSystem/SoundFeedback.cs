5f874c4e089c74eb3f9508d1017411927656eeaa
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-05-08 09:34:48 +0900

    feat: Boss Alert SFX

--- Assets/01.Scripts/FeedbackSystem/SoundFeedback.cs
+++ Assets/01.Scripts/FeedbackSystem/SoundFeedback.cs
@@ -6,14 +6,19 @@ namespace FeedbackSystem
     public class SoundFeedback : Feedback
     {
         [SerializeField] private SoundSO _soundData;
-
+        private SoundPlayer _soundPlayer;
         public override void CreateFeedback()
         {
-            SoundController.Instance.PlaySound(_soundData, transform.position);
+            _soundPlayer = SoundController.Instance.PlaySound(_soundData, transform.position);
+
         }
 
         public override void FinishFeedback()
         {
+            if (_soundData.loop)
+            {
+                _soundPlayer.SetForceOverSound();
+            }
         }
     }
 }
