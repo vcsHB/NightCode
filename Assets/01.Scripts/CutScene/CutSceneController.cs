90d9fffdffc4ac3e5316707c085259cccf459bf0
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-05-08 23:27:56 +0900

    feat: CutSceneController

---
+++ Assets/01.Scripts/CutScene/CutSceneController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace PerformanceSystem.CutScene
+{
+    [RequireComponent(typeof(PlayableDirector))]
+    public class CutSceneDirector : MonoBehaviour
+    {
+        private PlayableDirector _director;
+
+        private void Awake()
+        {
+            _director = GetComponentInChildren<PlayableDirector>();
+        }
+
+        public void PauseTimeline()
+        {
+            _director.Stop();
+        }
+
+        public void StartTimeline()
+        {
+            _director.Play();
+        }
+    }
+
+}
