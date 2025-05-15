9f0f9ddb5282cdad79baae785b9d6e1688015979
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-05-15 21:52:04 +0900

    refactor: Interact System

--- Assets/01.Scripts/InteractSystem/InteractTrigger.cs
+++ Assets/01.Scripts/InteractSystem/InteractTrigger.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net.Http.Headers;
 using UnityEngine;
 using UnityEngine.Events;
 namespace InteractSystem
@@ -6,6 +7,8 @@ namespace InteractSystem
 
     public class InteractTrigger : MonoBehaviour
     {
+        public UnityEvent OnInteractEnterEvent;
+        public UnityEvent OnInteractExitEvent;
         public UnityEvent OnInteractEvent;
         public event Action<InteractData> OnInteractDataEvent;
 
@@ -13,33 +16,54 @@ namespace InteractSystem
         [Header("Interact Setting")]
         [SerializeField] private float _detectRadius;
         [SerializeField] private LayerMask _targetLayer;
-        private Collider2D[] _targets; 
+        private Collider2D _currentTarget;
+        protected IInteractable _interactTarget;
 
-        public virtual void TryInteract()
+        public virtual void DetectTarget()
         {
-            Collider2D target =  Physics2D.OverlapCircle(transform.position, _detectRadius, _targetLayer);
-        }
-
-        public virtual void ForceInteract(Collider2D targetCollider)
-        {
-            if (targetCollider.TryGetComponent(out IInteractable interactable))
+            Collider2D target = Physics2D.OverlapCircle(transform.position, _detectRadius, _targetLayer);
+            if (target == null)
             {
-                InteractData data = new InteractData()
+                if (_currentTarget != null)
                 {
-                    interactorTrm = transform,
-                    interactDirection = targetCollider.transform.position - transform.position
-                };
-                interactable.Interact(data);
-                OnInteractEvent?.Invoke();
-                OnInteractDataEvent?.Invoke(data);
+                    _interactTarget.DetectExit();
+                    OnInteractExitEvent?.Invoke();
+                }
+                _currentTarget = null;
+                _interactTarget = null;
+                return;
+            }
+            if (_currentTarget == target) return;
+            if (target.TryGetComponent(out IInteractable interactTarget))
+            {
+                _currentTarget = target;
+                _interactTarget = interactTarget;
+
+                _interactTarget.DetectEnter();
+                OnInteractEnterEvent?.Invoke();
             }
         }
+
+        public void TryInteract()
+        {
+            if (_interactTarget == null) return;
+            InteractData data = new InteractData()
+            {
+                interactorTrm = transform,
+                interactDirection = _currentTarget.transform.position - transform.position
+            };
+            _interactTarget.Interact(data);
+            OnInteractEvent?.Invoke();
+            OnInteractDataEvent?.Invoke(data);
+        }
+
+
 
 #if UNITY_EDITOR
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = Color.magenta;
-
+            Gizmos.DrawWireSphere(transform.position, _detectRadius);
         }
 
 #endif
