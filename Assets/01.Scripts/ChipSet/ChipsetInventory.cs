f993b3fc57c3e87ae8a79358eda82fe682101276
Leooo <123617759+Leooo918@users.noreply.github.com>
2025-06-03 09:32:33 +0900

    Reapply "feat:inventory_container"
    This reverts commit def5cb034cc046c560261a62f2d6ad2d7a43307e.

--- Assets/01.Scripts/ChipSet/ChipsetInventory.cs
+++ Assets/01.Scripts/ChipSet/ChipsetInventory.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -8,6 +9,9 @@ namespace Chipset
 {
     public class ChipsetInventory : MonoBehaviour
     {
+        public event Action onInsertChipset;
+        public event Action onReturnChipset;
+
         public ChipsetGruopSO chipsetGroup;
         [SerializeField] private Vector2Int _inventorySize;
         [SerializeField] private ChipsetInventorySlot _slotPrefab;
@@ -15,6 +19,7 @@ namespace Chipset
 
         [SerializeField] private List<Vector2Int> _openSlot;
         [SerializeField] private List<Chipset> _exsistingChipset;
+
         private Dictionary<Chipset, List<Vector2Int>> _assignedChipsets = new Dictionary<Chipset, List<Vector2Int>>();
         private ChipsetInventorySlot[,] _slots;
         private Chipset[,] _chipsets;
@@ -28,7 +33,7 @@ namespace Chipset
 
         private void Awake()
         {
-            Init();
+            //Init();
         }
 
         #region Initialize
@@ -68,6 +73,11 @@ namespace Chipset
                 chipset.onSelectChipset += SelectChipset;
                 chipset.onPointerUpChipset += UnSelectChipset;
             });
+        }
+
+        public void AddAssignedChipset(Chipset chipset)
+        {
+            _exsistingChipset.Add(chipset);
         }
 
         #endregion
@@ -145,6 +155,7 @@ namespace Chipset
 
         private void ReturnChipsetToPrevPosition()
         {
+            onReturnChipset?.Invoke();
             List<Vector2Int> prevPositions = _selectedChipset.GetPrevPositions();
 
             if (prevPositions != null)
@@ -158,6 +169,7 @@ namespace Chipset
 
         private void InsertChipset(List<Vector2Int> positions, Vector2Int selectedPosition, Chipset chipset)
         {
+            onInsertChipset?.Invoke();
             positions.ForEach(prev => _chipsets[prev.x, prev.y] = chipset);
             _assignedChipsets.Add(chipset, positions.ToList());
 
