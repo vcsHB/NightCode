fd9dd16f848ece58582accbe690dc159eb42bc34
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-06-03 13:57:40 +0900

    feat: Cresent Weapon

--- Assets/01.Scripts/Agent/Player/WeaponSystem/Weapon/CresentWeapon.cs
+++ Assets/01.Scripts/Agent/Player/WeaponSystem/Weapon/CresentWeapon.cs
@@ -1,12 +1,70 @@
+using System;
+using Combat.Casters;
 using UnityEngine;
 namespace Agents.Players.WeaponSystem.Weapon
 {
 
     public class CresentWeapon : PlayerWeapon
     {
+        [SerializeField] private float _attackDuration = 1.5f;
+        [SerializeField] private SpriteRenderer _attackGuideVisual;
+        [SerializeField] private TrailRenderer _trailRenderer;
+        private Caster _caster;
+        private bool _isAttackEnabled;
+        private DamageCasterData _damageCasterData;
+        private float _currentAttackTime = 0f;
+
+        private void Awake()
+        {
+            _caster = GetComponentInChildren<Caster>();
+            _damageCasterData = new();
+        }
+        public override void Initialize(Player player)
+        {
+            base.Initialize(player);
+            _animationTrigger.OnRopeTurboEvent.AddListener(HandleAttack);
+            _animationTrigger.OnRopeRemoveEvent.AddListener(HandleRopeRemove);
+        }
+
+        private void HandleRopeRemove()
+        {
+            SetDisableWeapon();
+        }
+
         public override void HandleAttack()
         {
-            
+            _isAttackEnabled = true;
+            _attackGuideVisual.enabled = true;
+            _trailRenderer.Clear();
+            _trailRenderer.emitting = true;
+            _currentAttackTime = 0f;
+        }
+
+        public void SetDamage(float damage)
+        {
+            _damageCasterData.damage = damage;
+            _caster.SendCasterData(_damageCasterData);
+        }
+
+        private void Update()
+        {
+            if (_isAttackEnabled)
+            {
+                _currentAttackTime += Time.deltaTime;
+                _caster.Cast();
+
+                if (_currentAttackTime > _attackDuration)
+                    SetDisableWeapon();
+            }
+
+        }
+
+        private void SetDisableWeapon()
+        {
+            _isAttackEnabled = false;
+            _attackGuideVisual.enabled = false;
+            _trailRenderer.emitting = false;
         }
     }
+
 }
